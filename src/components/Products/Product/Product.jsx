85ab7f2e3cb971b50162ae5de521d14a08844071
zayin <zayinm300@gmail.com>
2023-11-12 20:34:37 +0000

    Updated Products components

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Card, CardMedia, CardContent, CardAction, Typography, IconButton } from "@mui/material"
-import { AddShoppingCart } from "@mui/icons";
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from "@mui/material"
+import { AddShoppingCart } from "@mui/icons-material";
+
+import useStyles from './styles';
 
 const Product = ({ product }) => {
+    const classes = useStyles();
+
     return (
         <Card>
-            <CardMedia className={ CallMissedSharp.media } image='' title={ product.name }></CardMedia>
+            <CardMedia className={ classes.media } image='' title={ product.name }></CardMedia>
             <CardContent  >
                 <div className={ classes.cardContent }>
                     <Typography variant='h5' gutterBottom>
@@ -16,7 +20,7 @@ const Product = ({ product }) => {
                     </Typography>
                 </div>
 
-                <Typography variant='h2' color="textSecondary">
+                <Typography variant='body2' color="textSecondary">
                     { product.description }
                 </Typography>
             </CardContent>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import Grid from "@mui/material";
+import { Grid } from "@mui/material";
+
+import Product from './Product/Product';
 
 const products = [
     { id: 1, name: "Shoe", description: "Running Shoes", price: "$50" },
@@ -8,15 +10,17 @@ const products = [
 ]
 
 const Products = () => {
-    <main>
-        <Grid container justify="center" spacing={ 4 }>
-            { products.map((product) => (
-                <Grid item key={ product.id } xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 }>\
-                    <Product product={ product } />
-                </Grid>
-            )) }
-        </Grid>
-    </main>
+    return (
+        <main>
+            <Grid container justify="center" spacing={ 4 }>
+                { products.map((product) => (
+                    <Grid item key={ product.id } xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 }>
+                        <Product product={ product } />
+                    </Grid>
+                )) }
+            </Grid>
+        </main>
+    );
 }
 
 export default Products;
